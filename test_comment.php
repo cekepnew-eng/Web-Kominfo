77cd29302ee70ccc4c4a264b2dbd269e9fec043c
cekepnew-eng <cekepnew@gmail.com>
2026-07-06 08:54:36 +0700

    chore: remove test files completely

--- test_comment.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'includes/db.php';
-$conn = db_get_conn();
-$sql = "INSERT INTO report_comments (report_id, commenter_name, comment_text, is_admin, parent_id) VALUES (1, 'Test', 'Test comment', 0, NULL)";
-if ($conn->query($sql)) {
-    echo "Success\n";
-} else {
-    echo "Error: " . $conn->error . "\n";
-}
